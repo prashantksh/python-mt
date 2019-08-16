68dac6a8c0928419f157260db4b8db0ba60673c6
vivekanandpv-mac <vivekanandpv@outlook.com>
2019-08-16 15:46:40 +0530

    added thread 2 in mt01

--- mt01.py
+++ mt01.py
@@ -3,11 +3,11 @@ import threading
 import time
 
 
-def foo(thread_name):
+def foo(thread_name, delay):
     logging.info(f"Run by {thread_name}")
 
     # Just halt this thread for 2 seconds
-    time.sleep(2)
+    time.sleep(delay)
     logging.info(f"{thread_name} ends")
 
 
@@ -19,9 +19,12 @@ if __name__ == "__main__":
     logging.info("Starting the main thread")
 
     # trailing comma in args tuple is required if there is only one argument
-    t1 = threading.Thread(target=foo, args=('thread 01',))
+    t1 = threading.Thread(target=foo, args=('thread 01', 2))
+    t2 = threading.Thread(target=foo, args=('thread 02', 4))
     logging.info("Invoking thread 01 from main thread")
     t1.start()
+    logging.info("Invoking thread 02 from main thread")
+    t2.start()
     logging.info("After the invocation of thread 01")
     logging.info("Main thread waits for the thread 01 to finish")
     t1.join()
